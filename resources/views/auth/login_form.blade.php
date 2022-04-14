8b1c2a17f793bbf3ea1d47c6c05780c4c4eef852
itsgitz <anggit.ginanjar@outlook.com>
2022-04-14 23:16:00 +0700

    style: Login padding

--- resources/views/auth/login_form.blade.php
+++ resources/views/auth/login_form.blade.php
@@ -1,15 +1,15 @@
-<div class="py-5"></div>
+<div class="py-4"></div>
 <div class="d-flex justify-content-center">
    <img src="/img/wings.svg" alt="logo-wings" width="150">
 </div>
-<div class="py-4"></div>
+<div class="py-2"></div>
 <div class="row">
    <div class="col-md-4 offset-md-4 rounded shadow-sm p-4">
       <div class="py-3"></div>
       <div class="d-flex justify-content-center">
          <h4 class="text-secondary">Login ke CCTV Data Inventory</h4>
       </div>
-      <div class="py-4"></div>
+      <div class="py-3"></div>
 
       <div id="auth-login-form" class="">
          <form action="{{ route('auth.login.post') }}" method="post">
