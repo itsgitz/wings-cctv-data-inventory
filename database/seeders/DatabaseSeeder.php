a91b51c2479e8fca4ef10b21636a0de4f34c28f4
itsgitz <anggit.ginanjar@outlook.com>
2022-05-27 17:12:15 +0700

    chore(database): Disable seeder for CCTV

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CctvSeeder::class,
+            //CctvSeeder::class,
             UserSeeder::class
         ]);
     }
