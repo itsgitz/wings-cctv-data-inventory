d3f6c5471698370c6793d57dcaeda309afc3dda8
itsgitz <anggit.ginanjar@outlook.com>
2022-06-26 11:23:09 +0700

    feat(middleware): Added user role middleware

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -1,5 +1,5 @@
 <div class="p-3 shadow-sm">
-   <nav class="navbar navbar-expand-lg navbar-light bg-wight">
+   <nav class="navbar navbar-expand-lg navbar-light bg-wight text-extra-sm">
       <div class="container">
       <a class="navbar-brand" href="{{ route('cctv.dashboard.get') }}">
          <img src="/img/wings.svg" alt="wings-logo" width="100">
@@ -15,30 +15,43 @@
          </ul>
          <div class="d-flex">
             <ul class="navbar-nav">
-               <li class="nav-item">
+               <li class="nav-item px-2">
                   <a class="nav-link fw-light" href="{{ route('cctv.dashboard.get') }}">
                      <i class="fas fa-chalkboard"></i> Dashboard
                   </a>
                </li>
                @if (Auth::user()->role == 'admin')
-               <li class="nav-item">
+               <li class="nav-item px-2">
                   <a class="nav-link fw-light" href="{{ route('cctvs.create') }}">
                      <i class="fas fa-camera"></i> Tambah CCTV</a>
                </li>
-               <li class="nav-item">
+               <li class="nav-item px-2">
                   <a class="nav-link fw-light" href="{{ route('users.index') }}">
                      <i class="fas fa-users"></i> User Management
                   </a>
                </li>
                @endif
-               <li class="nav-item">
-                  <a class="nav-link fw-light" href="{{ route('auth.logout.get') }}">
-                     <i class="fas fa-sign-out-alt"></i> Logout
+               <li class="nav-item px-2 dropdown">
+                  <a class="nav-link fw-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                     <i class="fas fa-user-circle"></i> {{ Auth::user()->name }}
                   </a>
+
+                  <ul class="dropdown-menu text-extra-sm" aria-labelledby="dropdownMenuLink">
+                     <li>
+                        <a class="fw-light dropdown-item" href="{{ route('users.edit', ['user' => Auth::id()]) }}">
+                           <i class="fas fa-user-edit"></i> Settings
+                        </a>
+                     </li>
+                     <li>
+                        <a class="fw-light dropdown-item" href="{{ route('auth.logout.get') }}">
+                           <i class="fas fa-sign-out-alt"></i> Logout
+                        </a>
+                     </li>
+                  </ul>
                </li>
             </ul>
          </div>
        </div>
-     </div>
+      </div>
    </nav>
 </div>
