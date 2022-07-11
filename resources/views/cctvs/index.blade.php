6f4176c9a9724a27040a29913e77ffa9699b31c6
itsgitz <anggit.ginanjar@outlook.com>
2022-07-11 10:00:17 +0700

    feat(view): Added title for group

--- resources/views/cctvs/index.blade.php
+++ resources/views/cctvs/index.blade.php
@@ -6,6 +6,7 @@
 <div id="cctv-index">
    @include ('shared.search_form')
 
+   @include ('cctvs.title')
    <div class="shadow-sm p-3">
       @include ('shared.message')
       @include ('cctvs.table_dashboard')
---
+++ resources/views/cctvs/title.blade.php
@@ -0,0 +1,4 @@
+<div class="shadow-sm p-3">
+   <h1>{{ Request::input('group') }}</h1>
+</div>
+<div class="py-3"></div>
